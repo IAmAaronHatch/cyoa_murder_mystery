ab4d448d8ca86235c942246073db5fa90e6e1be7
Aaron Hatch <aaron@aarons-mbp.attlocal.net>
2019-05-31 15:05:30 -0700

    weird bug fixes needed

--- script.js
+++ script.js
@@ -3,17 +3,20 @@
 // const i = 0;
 // const speed = 50;
 
+var i = 0;
+var speed = 50;
+
 // // SCRIPT
-// const a1s1t1 = 'Hello World, this is the first prompt';
+const a1s1t1 = 'Hello World, this is the first prompt';
 // const a1s1t2 = 'This is the continuation prompt that follows, so number two';
 
-// const write_a1s1t1 = () => {
-//     if (i < a1s1t1.length) {
-//         document.getElementById("introPrompt-1").innerHTML += a1s1t1.charAt(i);
-//         i++;
-//         setTimeout(write_a1s1t1, speed);
-//     }
-// }
+const write_a1s1t1 = () => {
+    if (i < a1s1t1.length) {
+        document.getElementById("introPrompt-1").innerHTML += a1s1t1.charAt(i);
+        i++;
+        setTimeout(write_a1s1t1, speed);
+    }
+}
 // const write_a1s1t2 = () => {
 //     if (i < a1s1t2.length) {
 //         document.getElementById("introPrompt-2").innerHTML += a1s1t2.charAt(i);
@@ -21,15 +24,3 @@
 //         setTimeout(write_a1s1t2, speed);
 //     }
 // }
-
-var i = 0;
-var txt = 'Lorem ipsum dummy text blabla.';
-var speed = 50;
-
-function typeWriter() {
-    if (i < txt.length) {
-        document.getElementById("demo").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-    }
-}
