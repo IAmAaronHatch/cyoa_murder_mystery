089493282b87010d16b77d96630e84cb127d90e5
Aaron Hatch <aaron@Aarons-MacBook-Pro.local>
2019-06-24 19:26:01 -0700

    added func start to fix race conditions

--- js/script.js
+++ js/script.js
@@ -36,9 +36,8 @@ submitNameBtn.addEventListener('click', function () {
     userNameInput.style.display = 'none';
     submitNameBtn.style.display = 'none';
 
+    start();
 
-
-    mainText.text1.createDisplay();
 });
 gameContainer.appendChild(submitNameBtn);
 
@@ -82,77 +81,90 @@ const array = [];
 
 // let currentQuestion = 'openDoor';
 
+let mainText;
 
-const mainText = {
-    text1: {
-        dialogue: `It is a dark and stormy night. You, ${userName} look down at your invitation you had recently recieved.`,
-        next: function() {
-            game.text2.createDisplay();
+function start () {
+
+    
+    mainText = {
+        text1: {
+            dialogue: `It is a dark and stormy night. You, ${userName} look down at your invitation you had recently recieved.`,
+            next: function () {
+                game.text2.createDisplay();
+            },
+            createDisplay() {
+                document.getElementById('question').innerText = game.text1.dialogue;
+
+                options.innerHTML = `<li onclick="game['text1'].next()"> > </li>`
+            }
         },
-        createDisplay(){
-            document.getElementById('question').innerText = game['text1'].dialogue;
+        text2: {
+            dialogue: `The invitation informed ${userName} of a dinner party being held tonight. "I wonder who else is going to show up?", you question.`,
+            next: function () {
+                game.text3.createDisplay();
+            },
+            createDisplay() {
+                document.getElementById('question').innerText = game.text2.dialogue;
 
-            console.log('username', userName)
-            options.innerHTML = `<li onclick="game['text1'].next()"> > </li>`
+                options.innerHTML = `<li onclick="game['text2'].next()"> > </li>`
+            }
+        },
+        text3: {
+            dialogue: `You step out of the car. The rain is persistant, coming down in sheets. You glance upon the manner that looms infront ahead. A large mansion, beautifully crafter, large windows with light pouring out to the darkness.`,
+            next: function () {
+                game.openDoor.createDisplay();
+            },
+            createDisplay() {
+                document.getElementById('question').innerText = game.text3.dialogue;
+
+                options.innerHTML = `<li onclick="game['text3'].next()"> > </li>`
+            }
+        },
+        openDoorChoice: {
+            dialogue: `You unfortunately didn't bring an umbrella, so you quickly run up to the front door. There is a letter on the ground. Do you knock on the door and pick up the letter or do you open the front door?`,
+            knock: function () {
+                game.knockFD.createDisplay();
+            },
+            open: function () {
+                game.openFD.createDisplay();
+            },
+            createDisplay(optionText) {
+                document.getElementById('question').innerText = game.openDoor.dialogue;
+
+                options.innerHTML = `<li onclick="game['openDoor'].knock()"> Knock on the door and pick up letter </li>
+            <li onclick="game['openDoor'].open()"> Open the door </li>`;
+
+                options.querySelectorAll('li');
+            }
+        },
+        knockOnFrontDoor: {
+            dialogue: `You knock on the door and pick up the letter`,
+            pickUpLetter: function () {
+                game.enterFD.createDisplay();
+                document.getElementById('inv');
+
+            }
         }
-    },
-    text2: {
-        dialogue: `The invitation informed ${userName} of a dinner party being held tonight. "I wonder who else is going to show up?", you question.`,
-        next: function () {
-            game.text3.createDisplay();
-        },
-        createDisplay() {
-            document.getElementById('question').innerText = game['text2'].dialogue;
 
-            options.innerHTML = `<li onclick="game['text2'].next()"> > </li>`
-        }
-    },
-    text3: {
-        dialogue: `You step out of the car. The rain is persistant, coming down in sheets. You glance upon the manner that looms infront ahead. A large mansion, beautifully crafter, large windows with light pouring out to the darkness.`,
-        next: function () {
-            game.openDoor.createDisplay();
-        },
-        createDisplay() {
-            document.getElementById('question').innerText = game['text3'].dialogue;
+    }
 
-            options.innerHTML = `<li onclick="game['text3'].next()"> > </li>`
-        }
-    },
-    openDoorChoice: {
-        question: `You unfortunately didn't bring an umbrella, so you quickly run up to the front door. Do you knock or do you open the front door?`,
-        knock: function () {
-            game.knockOnFrontDoor.createDisplay();
-        },
-        open: function () {
-            game.openWithoutKnock.createDisplay();
-        },
-        optionText: `<li onclick="game['openDoor'].knock()"> Knock on the door </li>
-        <li onclick="game['openDoor'].open()"> Open the door </li>`,
-        createDisplay(optionText) {
-            document.getElementById('question').innerText = game['openDoor'].question;
 
-            options.innerHTML = mainText.openDoorChoice.optionText;
-
-            options.querySelectorAll('li');
-        }
-    },
+    const game = {
+        inventory: [],
+        text1: mainText.text1,
+        text2: mainText.text2,
+        text3: mainText.text3,
+        openDoor: mainText.openDoorChoice,
+        knockFD: mainText.knockOnFrontDoor,
+        openFD: mainText.openFrontDoor
+    };
+    mainText.text1.createDisplay();
+    
     
 }
 
 
-// array.push(obj);
 
-const game = {
-    inventory: [],
-    text1: mainText.text1,
-    text2: mainText.text2,
-    text3: mainText.text3,
-    openDoor: mainText.openDoorChoice,
-};
-
-
-// on login, once you have finished login, to submit the input value into the user variable, call this function to start the text!
-// mainText.text1.createDisplay();
 
 
 
@@ -174,20 +186,27 @@ const game = {
 
 const blueBorder = document.createElement('div');
 blueBorder.classList.add('blue-border-btn');
-blueBorder.addEventListener('click', function(){
-    gameContainer.style.border = 'lightblue 2px solid';
+blueBorder.addEventListener('click', function () {
+    let bs = 'lightblue 4px solid'
+    gameContainer.style.border = bs;
+    decisionContainer.style.border = bs;
 });
 
 const redBorder = document.createElement('div');
 redBorder.classList.add('red-border-btn');
 redBorder.addEventListener('click', function () {
-    gameContainer.style.border = 'lightcoral 2px solid'
+    let bs = 'lightcoral 4px solid';
+    gameContainer.style.border = bs;
+    decisionContainer.style.border = bs;
 });
 
 const greenBorder = document.createElement('div');
 greenBorder.classList.add('green-border-btn');
 greenBorder.addEventListener('click', function () {
-    gameContainer.style.border = 'lightgreen 2px solid'
+    let bs = 'lightgreen 4px solid';
+
+    gameContainer.style.border = bs;
+    decisionContainer.style.border = bs;
 });
 
 characterContainer.appendChild(blueBorder);
